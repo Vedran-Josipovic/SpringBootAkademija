8521a0e1ea0f076bde52a50e2df7afab23322fbd
Vedran-Josipovic <vedranjosipovic.2014@gmail.com>
2023-06-17 11:22:38 +0200

    update

--- src/main/java/hr/kingict/akademija2023/springbootakademija/controller/DashboardController.java
+++ src/main/java/hr/kingict/akademija2023/springbootakademija/controller/DashboardController.java
@@ -1,14 +1,18 @@
 package hr.kingict.akademija2023.springbootakademija.controller;
 
+import hr.kingict.akademija2023.springbootakademija.services.DashboardService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashboardController {
+    private DashboardService dashboardService;
+
+
 
     @GetMapping(value = "/dashboard")
     public String getDashboard(){
-        return "dashboard.html";
+        return dashboardService.getDashboard();
     }
 
 
--- src/main/java/hr/kingict/akademija2023/springbootakademija/services/implementation/DashboardServiceTest.java
+++ src/main/java/hr/kingict/akademija2023/springbootakademija/services/implementation/DashboardServiceTest.java
@@ -1,12 +1,10 @@
 package hr.kingict.akademija2023.springbootakademija.services.implementation;
 
-
 import hr.kingict.akademija2023.springbootakademija.services.DashboardService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DashboardServiceTest implements DashboardService {
-
     @Override
     public String getDashboard() {
         return "dashboardTest.html";
