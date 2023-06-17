cfa337942784d0f3bb9a73ff8108bdb65065fa5f
Vedran-Josipovic <vedranjosipovic.2014@gmail.com>
2023-06-17 11:57:14 +0200

    update

---
+++ src/main/java/util/FilenameUtils.java
@@ -0,0 +1,12 @@
+package util;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FilenameUtils {
+
+    public String getSuffixSeparator(){
+        return ".";
+    }
+
+}
