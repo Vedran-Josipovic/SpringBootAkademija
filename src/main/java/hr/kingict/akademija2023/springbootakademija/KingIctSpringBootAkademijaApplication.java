5379b6c2e9ae53c09a920f2346522c3347d59feb
Vedran-Josipovic <vedranjosipovic.2014@gmail.com>
2023-06-17 10:33:50 +0200

    Gaming

---
+++ src/main/java/hr/kingict/akademija2023/springbootakademija/KingIctSpringBootAkademijaApplication.java
@@ -0,0 +1,13 @@
+package hr.kingict.akademija2023.springbootakademija;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingIctSpringBootAkademijaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KingIctSpringBootAkademijaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/kingict/akademija2023/springbootakademija/KingIctSpringBootAkademijaApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.kingict.akademija2023.springbootakademija;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KingIctSpringBootAkademijaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
