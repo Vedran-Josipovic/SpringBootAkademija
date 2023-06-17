0f080b109cc03e0de1503fad46736e49d690ba4a
Vedran-Josipovic <vedranjosipovic.2014@gmail.com>
2023-06-17 11:57:14 +0200

    update

--- src/main/java/hr/kingict/akademija2023/springbootakademija/controller/DashboardController.java
+++ src/main/java/hr/kingict/akademija2023/springbootakademija/controller/DashboardController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import util.FilenameUtils;
 
 @Controller
 public class DashboardController {
@@ -17,9 +18,12 @@ public class DashboardController {
     @Qualifier("html")
     private PageSuffixService pageSuffixService;
 
+    @Autowired
+    private FilenameUtils filenameUtils;
+
     @GetMapping(value = "/dashboard")
     public String getDashboard(){
-        return dashboardService.getDashboard() + "." + pageSuffixService.getSuffix();
+        return dashboardService.getDashboard() + filenameUtils.getSuffixSeparator() + pageSuffixService.getSuffix();
     }
 
 
